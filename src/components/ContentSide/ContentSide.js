31586f9f488347cae6efb0c679836fcb97a7a748
fav757 <fav755@gmail.com>
2020-08-21 18:57:16 +0300

    add Home page component

--- src/components/ContentSide/ContentSide.js
+++ src/components/ContentSide/ContentSide.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Header from '../Header/Header';
+import HomePage from './HomePage';
 
 const ContentSideStyled = styled.div`
   padding: 2rem;
@@ -10,6 +11,7 @@ function ContentSide() {
   return (
     <ContentSideStyled>
       <Header />
+      <HomePage />
     </ContentSideStyled>
   );
 }
---
+++ src/components/ContentSide/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Alexandr Sapalsky</h1>
+      <h2>Junior Front-end developer</h2>
+      <p>
+        Welcome to my site. Here you can find out who I am and what I can do.
+      </p>
+    </div>
+  );
+}
+
+export default HomePage;
