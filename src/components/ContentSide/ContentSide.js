0ca6dc5b78ddeacf0d201074bb5eb6c094703547
fav757 <fav755@gmail.com>
2020-08-21 18:28:45 +0300

    add icons and styles for content side component

--- src/components/ContentSide/ContentSide.js
+++ src/components/ContentSide/ContentSide.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const ContentSideStyled = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem;
+`;
 
 function ContentSide() {
   return (
-    <div>
+    <ContentSideStyled>
       <nav>
         <i className='fas fa-arrow-left'></i>
         <h2>Home</h2>
       </nav>
       <div>
-        <i>lang icon</i>
-        <i>theme icon</i>
+        <i className='fas fa-lightbulb'></i>
+        <i className="fas fa-globe"></i>
       </div>
-    </div>
+    </ContentSideStyled>
   );
 }
 
