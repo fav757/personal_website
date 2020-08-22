2af01429ac9034fd743cf53be80020369f71a3ff
fav757 <fav755@gmail.com>
2020-08-22 12:53:32 +0300

    add theme colors for global styles

--- src/App.js
+++ src/App.js
@@ -24,18 +24,28 @@ const GlobalStyles = createGlobalStyle`
   body {
     height: 100%;
     width: 100%;
-    background: linear-gradient(60deg, #312664 0%, #1A9E6F 100%) no-repeat;
+    background: ${(props) =>
+      props.theme.mode === 'dark'
+        ? 'linear-gradient(60deg, #312664 0%, #1A9E6F 100%) no-repeat'
+        : 'rgb(240, 240, 240)'};
     display: flex;
     align-items: center;
     justify-content: center;
+    transition: 1s;
   }
 
   #root {
     display: flex;
     width: 992px;
     height: 576px;
-    background: linear-gradient(90deg, #373648 0%, #383556 100%);
-    box-shadow: 0px 0px 7px black;
+    background: ${(props) =>
+      props.theme.mode === 'dark'
+        ? 'linear-gradient(90deg, #373648 0%, #383556 100%)'
+        : 'white'} ;
+    box-shadow: ${(props) =>
+      props.theme.mode === 'dark'
+        ? '0px 0px 7px black'
+        : '0px 0px 7px lightgray'};
   }
 `;
 
