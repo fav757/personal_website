c2ae5631afaa9bdf8974dc9edc76c86dda21d66a
fav757 <fav755@gmail.com>
2020-08-23 16:04:36 +0300

    add nav back button

--- src/components/Header/Navigation.js
+++ src/components/Header/Navigation.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import NavModalMenu from './NavModalMenu';
+import { useHistory } from 'react-router-dom';
 
 const NavigationStyled = styled.nav`
   display: flex;
@@ -11,13 +12,31 @@ const NavigationStyled = styled.nav`
   }
 `;
 
+const pageHistory = [];
+
 function Navigation() {
   const [showMenu, setShowMenu] = useState(false);
+  const history = useHistory();
+
+  useEffect(() => {
+    pageHistory.push(history.location.pathname);
+  }, [history.location.pathname]);
+
   const handleClick = () => setShowMenu(!showMenu);
+  const backHistory = () => {
+    if (pageHistory[pageHistory.length - 2]) {
+      pageHistory.pop();
+      history.goBack();
+    } else {
+      if (window.confirm('Do you want to leave the site?')) {
+        history.goBack();
+      }
+    }
+  };
 
   return (
     <NavigationStyled>
-      <i className='fas fa-arrow-left'></i>
+      <i onClick={backHistory} className='fas fa-arrow-left'></i>
       <div>
         <h2 onClick={handleClick}>Home</h2>
         {showMenu && <NavModalMenu setShowMenu={setShowMenu} />}
