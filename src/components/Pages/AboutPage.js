97d619f1f740d21e22e29a308d967c7a27e9ceed
fav757 <fav755@gmail.com>
2020-08-26 13:12:28 +0300

    translate about page

--- src/components/Pages/AboutPage.js
+++ src/components/Pages/AboutPage.js
@@ -1,37 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
 import basicPageStyle from './PageBasicStyle';
+import { useTranslation } from 'react-i18next';
 
 const AboutPageStyled = styled(basicPageStyle)``;
 
 function AboutPage() {
-  const birthday = new Date('09/12/1997');
-  const milisecsInYear = 1000 * 60 * 60 * 24 * 365.25;
-  const age = Math.floor((new Date() - birthday) / milisecsInYear);
+  const { t } = useTranslation();
 
   return (
     <AboutPageStyled>
-      <h2>Main</h2>
-      <p>
-        I am a {age} year beginner web developer from Kharkiv who is interested
-        in web technologies and right now I want to start my Front-end career,
-        work on real projects, improving my skills in practice.
-      </p>
+      <h2>{t('aboutPage main header')}</h2>
+      <p>{t('aboutPage main')}</p>
 
-      <h2>Education</h2>
-      <p>
-        I study at Kharkiv polytechnical institute at the Faculty of Engineering
-        and Physics, specialty "Computer Science". In 2019 I received a
-        bachelor's degree. At this moment I am continuing my education in the
-        sixth year to get a master's degree.
-      </p>
+      <h2>{t('aboutPage education header')}</h2>
+      <p>{t('aboutPage education')}</p>
 
-      <h2>Experience</h2>
+      <h2>{t('aboutPage experience header')}</h2>
       <p>
-        I did my own study of Front-end basics using resources like MDN and
-        learn.javascript.ru. Also was trained in Epam courses in the "Front-end
-        / JavaScript" direction. Made several little projects like notes
-        aplication or spends manager. My code is available on{' '}
+        {t('aboutPage experience')}
         <a
           href='https://github.com/fav757/'
           target='_blank'
